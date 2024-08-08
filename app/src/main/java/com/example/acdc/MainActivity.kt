d7cedd6945596f52c171493bc6136aa73b37020b
LukaszSztajerowski <lukaszsztajerowski@gmail.com>
2024-08-08 16:58:03 +0200

    stworzenie struktury

--- app/src/main/java/com/example/acdc/MainActivity.kt
+++ app/src/main/java/com/example/acdc/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.acdc
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import com.example.acdc.ui.theme.AddictionCravingDiaryCalendarTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +23,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AddictionCravingDiaryCalendarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "lista objawów")
+                    }
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "dodaj objawy")
+                    }
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "edytuj objawy")
+                    }
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "wykonaj dzienniczek")
+                    }
+                    
                 }
             }
         }
@@ -31,17 +48,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyHomeActivityView(){
+    val context: Context = LocalContext.current
+    val intentHome: Intent = Intent(context, HomeActivity::class.java)
+    val intentSymptomListActivity: Intent = Intent(context, SymptomListActivity::class.java)
+    val intentAddSymptomActivity: Intent = Intent(context, AddSymptomActivity::class.java)
+    val intent: Intent = Intent(context, HomeActivity::class.java)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AddictionCravingDiaryCalendarTheme {
-        Greeting("Android")
+
+
+    
+    Button(onClick = { startActivity()}) {
+        Text(text = )
+        
     }
+        
 }
